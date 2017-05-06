2b59937ccf7720f4ee6653cf07da2ab2f6d05d9c
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-05-06 17:16:12 +0700

    fix scope authorization

--- src/components/SpotifyLogin.jsx
+++ src/components/SpotifyLogin.jsx
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 export default class SpotifyLogin extends Component {
   loginSpotify() {
-    const scopes = 'streaming user-read-email';
+    const scopes = ['user-read-email', 'user-read-recently-played', 'user-modify-playback-state'].join(' ');
     const redirectURI = 'http://localhost:9000';
     const clientID = '0856b68f7de34d5a8b885d6a3db52c5b';
-    const authorizePath = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=code&redirect_uri=${encodeURIComponent(redirectURI)}&${encodeURIComponent(scopes)}`
+    const authorizePath = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=code&redirect_uri=${encodeURIComponent(redirectURI)}&scope=${encodeURIComponent(scopes)}`
 
     window.location = authorizePath;
   }
