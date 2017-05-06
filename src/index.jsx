c20af183028777bec4757c81ca975efd159006fe
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-05-06 16:40:06 +0700

    redirect after get auth code

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+import queryString from 'query-string';
 
 import Main from './layouts/Main';
 
-render(
-  <Main />,
-  document.getElementById('root')
-);
+const params = queryString.parse(window.location.search);
+
+if (params.code) {
+  window.localStorage.setItem('authCode', params.code);
+  window.location = '/';
+} else {
+  render(
+    <Main />,
+    document.getElementById('root')
+  );
+}
